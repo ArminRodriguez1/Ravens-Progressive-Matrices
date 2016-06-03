0f8010bd51d5adb4c6793bf30bfc46ebd229bdda
prshrestha <cpflyingduck@gmail.com>
2016-06-02 20:55:57 -0400

    Solves RPMs with one objects
    Can solve RPMs with one object that hasn’t been transformed a lot.

---
+++ Copy of Project-Code-Java/ravensproject/OneObject.java
@@ -0,0 +1,14 @@
+package ravensproject;
+
+public class OneObject {
+	
+	private RavensProblem problem;
+	private int ans = -1;
+	
+	public OneObject(RavensProblem problem) {
+		this.problem = problem;
+	}
+	
+	
+
+}
