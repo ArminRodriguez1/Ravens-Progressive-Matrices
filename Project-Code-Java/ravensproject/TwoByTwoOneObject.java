738b90173e1a53cae2ff86cfe4ba99cd192a0e35
prshrestha <cpflyingduck@gmail.com>
2016-06-06 21:26:52 -0400

    2 by 2 null pointer in server

--- Project-Code-Java/ravensproject/TwoByTwoOneObject.java
+++ Project-Code-Java/ravensproject/TwoByTwoOneObject.java
@@ -16,6 +16,17 @@ public class TwoByTwoOneObject {
 
 	private RavensProblem problem;
 	private int tempAns;
+	
+	//hash map for Ravens Figure
+	HashMap<String, RavensFigure> RF;
+	//Ravens figure
+	RavensFigure RFA, RFB, RFC, RF1, RF2, RF3, RF4, RF5, RF6;
+	//Hash map of Ravens object
+	HashMap<String, RavensObject> ROA, ROB, ROC, RO1, RO2, RO3, RO4, RO5, RO6;
+	//Ravens object
+	RavensObject ROa, ROb,ROc, RO1d, RO2e, RO3f, RO4g, RO5h, RO6i;
+	//attributes for Ravens object
+	HashMap<String, String> ROaa, ROba,ROca, RO1da, RO2ea, RO3fa, RO4ga, RO5ha, RO6ia;
 
 	public TwoByTwoOneObject(RavensProblem problem) {
 		this.problem = problem;
@@ -25,64 +36,64 @@ public class TwoByTwoOneObject {
 
 		//check if the problem has verbal or not
 		if (problem.hasVerbal()) {
-			HashMap<String, RavensFigure> RF = problem.getFigures();
+			RF = problem.getFigures();
 
 			//these are the Ravens Figure from the problem
-			RavensFigure RFA = RF.get("A");
-			RavensFigure RFB = RF.get("B");
-			RavensFigure RFC = RF.get("C");
+			RFA = RF.get("A");
+			RFB = RF.get("B");
+			RFC = RF.get("C");
 
 			//these are the Ravens Figure from the solution
-			RavensFigure RF1 = RF.get("1");
-			RavensFigure RF2 = RF.get("2");
-			RavensFigure RF3 = RF.get("3");
-			RavensFigure RF4 = RF.get("4");
-			RavensFigure RF5 = RF.get("5");
-			RavensFigure RF6 = RF.get("6");
+			RF1 = RF.get("1");
+			RF2 = RF.get("2");
+			RF3 = RF.get("3");
+			RF4 = RF.get("4");
+			RF5 = RF.get("5");
+			RF6 = RF.get("6");
 
 			//get the Ravens Figure from the problem figures
-			HashMap<String, RavensObject> ROA = RFA.getObjects();
-			HashMap<String, RavensObject> ROB = RFB.getObjects();
+			ROA = RFA.getObjects();
+			ROB = RFB.getObjects();
 			HashMap<String, RavensObject> ROC = RFC.getObjects();   
 
 			//get the Ravens Figure from the problem solutions
-			HashMap<String, RavensObject> RO1 = RF1.getObjects();
-			HashMap<String, RavensObject> RO2 = RF2.getObjects();
-			HashMap<String, RavensObject> RO3 = RF3.getObjects();
-			HashMap<String, RavensObject> RO4 = RF4.getObjects();
-			HashMap<String, RavensObject> RO5 = RF5.getObjects();
-			HashMap<String, RavensObject> RO6 = RF6.getObjects();
+			RO1 = RF1.getObjects();
+			RO2 = RF2.getObjects();
+			RO3 = RF3.getObjects();
+			RO4 = RF4.getObjects();
+			RO5 = RF5.getObjects();
+			RO6 = RF6.getObjects();
 
 			//get the Ravens Objects from Ravens Figure
-			RavensObject ROa = ROA.get("a");    	
-			RavensObject ROb = ROB.get("b");    	
-			RavensObject ROc = ROC.get("c");
+			ROa = ROA.get("a");    	
+			ROb = ROB.get("b");    	
+			ROc = ROC.get("c");
 
 			//System.out.println("name of the ravens object ROa: " + ROa.getAttributes().containsValue("square"));
 
 			//get the Ravens Objects from the Ravens Figure in solution
-			RavensObject RO1d = RO1.get("d");
-			RavensObject RO2e = RO2.get("e");
-			RavensObject RO3f = RO3.get("f");
-			RavensObject RO4g = RO4.get("g");
-			RavensObject RO5h = RO5.get("h");
-			RavensObject RO6i = RO6.get("i");
+			RO1d = RO1.get("d");
+			RO2e = RO2.get("e");
+			RO3f = RO3.get("f");
+			RO4g = RO4.get("g");
+			RO5h = RO5.get("h");
+			RO6i = RO6.get("i");
 
 			//get attributes for the Ravens Objects
-			HashMap<String, String> ROaa = ROa.getAttributes();
-			HashMap<String, String> ROba = ROb.getAttributes();
-			HashMap<String, String> ROca = ROc.getAttributes();
+			ROaa = ROa.getAttributes();
+			ROba = ROb.getAttributes();
+			ROca = ROc.getAttributes();
 
 			//System.out.println("ROaa: " + ROaa);
 			//System.out.println("one object, ROca: " + ROca);
 
 			//get attributes for the solution objects
-			HashMap<String, String> RO1da = RO1d.getAttributes();
-			HashMap<String, String> RO2ea = RO2e.getAttributes();
-			HashMap<String, String> RO3fa = RO3f.getAttributes();
-			HashMap<String, String> RO4ga = RO4g.getAttributes();
-			HashMap<String, String> RO5ha = RO5h.getAttributes();
-			HashMap<String, String> RO6ia = RO6i.getAttributes();
+			RO1da = RO1d.getAttributes();
+			RO2ea = RO2e.getAttributes();
+			RO3fa = RO3f.getAttributes();
+			RO4ga = RO4g.getAttributes();
+			RO5ha = RO5h.getAttributes();
+			RO6ia = RO6i.getAttributes();
 			
 			//System.out.println("Compare ROca to RO1da in first class: " + ROca.equals(RO1da) );
 
