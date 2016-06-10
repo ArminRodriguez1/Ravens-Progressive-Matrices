4a0c49940cf828c4a288010a04796385ab7414b3
prshrestha <cpflyingduck@gmail.com>
2016-06-09 21:54:28 -0400

    Code clean up, 9/12

--- Project-Code-Java/ravensproject/Agent.java
+++ Project-Code-Java/ravensproject/Agent.java
@@ -13,25 +13,25 @@ import java.util.HashMap;
 /**
  * Your Agent for solving Raven's Progressive Matrices. You MUST modify this
  * file.
- * 
+ *
  * You may also create and submit new files in addition to modifying this file.
- * 
+ *
  * Make sure your file retains methods with the signatures:
  * public Agent()
  * public char Solve(RavensProblem problem)
- * 
+ *
  * These methods will be necessary for the project's main method to run.
- * 
+ *
  */
 public class Agent {
-	
+
 	/**
 	 * The default constructor for your Agent. Make sure to execute any
 	 * processing necessary before your Agent starts solving problems here.
-	 * 
+	 *
 	 * Do not add any variables to this signature; they will not be used by
 	 * main().
-	 * 
+	 *
 	 */
 	//private RavensProblem problem;
 	HashMap<String, RavensFigure> RF;
@@ -46,10 +46,10 @@ public class Agent {
 	 * The primary method for solving incoming Raven's Progressive Matrices.
 	 * For each problem, your Agent's Solve() method will be called. At the
 	 * conclusion of Solve(), your Agent should return an int representing its
-	 * answer to the question: 1, 2, 3, 4, 5, or 6. Strings of these ints 
+	 * answer to the question: 1, 2, 3, 4, 5, or 6. Strings of these ints
 	 * are also the Names of the individual RavensFigures, obtained through
 	 * RavensFigure.getName(). Return a negative number to skip a problem.
-	 * 
+	 *
 	 * Make sure to return your answer *as an integer* at the end of Solve().
 	 * Returning your answer as a string may cause your program to crash.
 	 * @param problem the RavensProblem your agent should solve
@@ -108,7 +108,7 @@ public class Agent {
 		TwoByTwoOneObject oneObject = new TwoByTwoOneObject(problem);
 		return oneObject.oneObjectRPM();
 	}
-	
+
 	/*
 	 * This method solves for 2 by 2 RPM that has two objects in a figure
 	 */
@@ -117,7 +117,7 @@ public class Agent {
 		TwoByTwoTwoObjects twoObjects = new TwoByTwoTwoObjects(problem);
 		return twoObjects.twoObjectRPM();
 	}
-	
+
 	//prints current date and time for the execution
 	//http://www.mkyong.com/java/java-how-to-get-current-date-time-date-and-calender/
 	private void printDateTime(){
